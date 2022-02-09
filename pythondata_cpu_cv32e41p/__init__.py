3bbc36663e4b5007f04ef04ec03767cf26d25263
LiteX Robot <robot@timvideos.us>
2022-02-09 18:31:08 +0000

    Updating pythondata-cpu-cv32e41p to 0.0.post1858
    Updated data to v0.0-1734-g4b6befd based on 4b6befd76b09ffd3294d5936b276891682b19c2e from https://github.com/openhwgroup/cv32e41p.
    > commit 4b6befd76b09ffd3294d5936b276891682b19c2e
    > Merge: f0353ff 188e8a5
    > Author: Pasquale Davide Schiavone <davide@openhwgroup.org>
    > Date:   Wed Feb 9 18:41:31 2022 +0100
    >
    >     Merge pull request #15 from abukharmeh/master
    >
    >     1) Update the assertions and tracer ifdefs
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e41p/__init__.py
+++ pythondata_cpu_cv32e41p/__init__.py
@@ -4,30 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e41p"
 
 # Module version
-version_str = "0.0.post1856"
-version_tuple = (0, 0, 1856)
+version_str = "0.0.post1858"
+version_tuple = (0, 0, 1858)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1856")
+    pversion = V("0.0.post1858")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1732"
-data_version_tuple = (0, 0, 1732)
+data_version_str = "0.0.post1734"
+data_version_tuple = (0, 0, 1734)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1732")
+    pdata_version = V("0.0.post1734")
 except ImportError:
     pass
-data_git_hash = "f0353ff7c0e7e9072b340eda9c6bc09477403867"
-data_git_describe = "v0.0-1732-gf0353ff"
+data_git_hash = "4b6befd76b09ffd3294d5936b276891682b19c2e"
+data_git_describe = "v0.0-1734-g4b6befd"
 data_git_msg = """\
-commit f0353ff7c0e7e9072b340eda9c6bc09477403867
+commit 4b6befd76b09ffd3294d5936b276891682b19c2e
+Merge: f0353ff 188e8a5
 Author: Pasquale Davide Schiavone <davide@openhwgroup.org>
-Date:   Tue Aug 31 17:15:06 2021 +0200
+Date:   Wed Feb 9 18:41:31 2022 +0100
 
-    update README with correct link (#9)
+    Merge pull request #15 from abukharmeh/master
+    
+    1) Update the assertions and tracer ifdefs
 
 """
 
