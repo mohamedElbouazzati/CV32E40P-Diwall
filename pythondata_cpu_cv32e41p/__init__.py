1227e0c90de7df4c063b629e73603e16f018a0f2
LiteX Robot <robot@timvideos.us>
2022-03-23 08:34:23 +0000

    Updating pythondata-cpu-cv32e41p to 0.0.post1866
    Updated data to v0.0-1740-gf1d5346 based on f1d53463befa1ab91c8b00ba537d2e0f09ab9882 from https://github.com/openhwgroup/cv32e41p.
    > commit f1d53463befa1ab91c8b00ba537d2e0f09ab9882
    > Author: Ibrahim Abu Kharmeh <ibrahim.abu.kharmeh@huawei.com>
    > Date:   Wed Mar 23 08:31:01 2022 +0000
    >
    >     Complete merging the decoder (#22)
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e41p/__init__.py
+++ pythondata_cpu_cv32e41p/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e41p"
 
 # Module version
-version_str = "0.0.post1865"
-version_tuple = (0, 0, 1865)
+version_str = "0.0.post1866"
+version_tuple = (0, 0, 1866)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1865")
+    pversion = V("0.0.post1866")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1739"
-data_version_tuple = (0, 0, 1739)
+data_version_str = "0.0.post1740"
+data_version_tuple = (0, 0, 1740)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1739")
+    pdata_version = V("0.0.post1740")
 except ImportError:
     pass
-data_git_hash = "f22dca3090069089cf17eb3d16c90918b7157130"
-data_git_describe = "v0.0-1739-gf22dca3"
+data_git_hash = "f1d53463befa1ab91c8b00ba537d2e0f09ab9882"
+data_git_describe = "v0.0-1740-gf1d5346"
 data_git_msg = """\
-commit f22dca3090069089cf17eb3d16c90918b7157130
+commit f1d53463befa1ab91c8b00ba537d2e0f09ab9882
 Author: Ibrahim Abu Kharmeh <ibrahim.abu.kharmeh@huawei.com>
-Date:   Mon Mar 7 16:20:15 2022 +0000
+Date:   Wed Mar 23 08:31:01 2022 +0000
 
-    Change PULP_ZFINX to ZFINX (#21)
+    Complete merging the decoder (#22)
 
 """
 
