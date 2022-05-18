678f0a42dd30442d0c9c86286e919212dfe6c125
LiteX Robot <robot@timvideos.us>
2022-05-18 18:16:47 +0000

    Updating pythondata-cpu-cv32e41p to 0.0.post1869
    Updated data to v0.0-1741-gbda060e based on bda060eee5bfb22fdbe55029c5e3cf1b62724fff from https://github.com/openhwgroup/cv32e41p.
    > commit bda060eee5bfb22fdbe55029c5e3cf1b62724fff
    > Author: Ibrahim Abu Kharmeh <ibrahim.abu.kharmeh@huawei.com>
    > Date:   Thu Apr 7 11:28:49 2022 +0100
    >
    >     Remove PULP build status from README (#25)
    >
    
    Updated using 0.0.post128 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e41p/__init__.py
+++ pythondata_cpu_cv32e41p/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e41p"
 
 # Module version
-version_str = "0.0.post1867"
-version_tuple = (0, 0, 1867)
+version_str = "0.0.post1869"
+version_tuple = (0, 0, 1869)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1867")
+    pversion = V("0.0.post1869")
 except ImportError:
     pass
 
@@ -32,11 +32,11 @@ Date:   Thu Apr 7 11:28:49 2022 +0100
 """
 
 # Tool version info
-tool_version_str = "0.0.post126"
-tool_version_tuple = (0, 0, 126)
+tool_version_str = "0.0.post128"
+tool_version_tuple = (0, 0, 128)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post126")
+    ptool_version = V("0.0.post128")
 except ImportError:
     pass
 
